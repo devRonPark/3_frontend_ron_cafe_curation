db9f7a7a618cd5f2785be17efc7da127377ca6ed
devPark <ronny_park@naver.com>
2022-03-10 15:52:48 +0900

    웹페이지 favicon 적용 시 serve-favicon 미들웨어 사용

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
+const favicon = require('serve-favicon');
+const path = require('path');
 
 const indexRouter = require('./src/routes/index.routes');
 
+app.use(express.static('public'));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
+  console.log('req: ', req);
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
