0f9e4135cc464d391fbbdc284b10c0e58a49ffde
Ron(Byungchan Park) <57528803+byungchanparkme@users.noreply.github.com>
2022-03-21 20:11:50 +0900

    CSS 클래스 및 모달창 제어함수 리팩토링
    * feat: 모바일 및 데스크탑 페이지에서만 존재하는 요소 구분 CSS 코드 작성
    * refactor: 헤더 아이콘 크기 조정
    * refactor: 필요없는 함수 제거

--- public/js/header.js
+++ public/js/header.js
@@ -38,19 +38,19 @@ const dongSelect = {
 };
 let dongDataForOptions;
 
+// 모달 창 열기 및 닫기
 const handleModalOpen = (evt, domElem) => {
   domElem.classList.toggle('active');
+  console.log(domElem.classList);
 };
-const handleNavModalOpen = evt => {
-  navModalOverlay.classList.toggle('active');
-};
-const handleSearchModalOpen = evt => {
-  searchModal.classList.toggle('active');
-};
+// 셀렉트 박스 선택 옵션들 활성화 여부 제어
 const handleSearchFilterOptionOpen = (evt, obj) => {
+  // 선택 옵션들 열기 및 닫기
   obj.list.classList.toggle('hide');
+  // 화살표 아이콘 회전
   obj.arrowIcon.classList.toggle('rotate');
 };
+// 선택 옵션들 중 하나 선택 시 이벤트 제어
 const handleSelectOption = (evt, obj) => {
   const target = evt.target;
   // 구 옵션을 선택한 경우
@@ -62,6 +62,7 @@ const handleSelectOption = (evt, obj) => {
     addDongOptionsByGuName(guName);
   }
 
+  // 선택된 옵션 텍스트가 선택되었음을 화면에 보여줌.
   obj.text.innerHTML = target.textContent;
   obj.list.classList.toggle('hide');
   obj.arrowIcon.classList.toggle('rotate');
@@ -92,12 +93,21 @@ const parseDongDataForOptions = async () => {
         dataForOptions[guName].push(dongName);
       }
     }
+    // 가공 결과 다음과 같은 데이터 형태를 가진다.
+    // {
+    //   [gu]: [ dongList ],
+    //   "종로구": [
+    //     "청운효자동", "사직동", "삼청동", "부암동", "평창동", "무악동", "교남동", "가회동", ...
+    //   ],
+    //   ...
+    // }
     return dataForOptions;
   } catch (err) {
     console.log(err);
   }
 };
 // 동 셀렉트 박스에 추가할 DOM 템플릿 정의하기
+// <li class="option">${dongName}</li>
 const makeDongOptionTemplate = dongName => {
   const li = document.createElement('li');
   li.className = 'option';
@@ -114,14 +124,21 @@ const addDongOptionsByGuName = guName => {
       dongOptionElem.addEventListener('click', evt =>
         handleSelectOption(evt, dongSelect),
       );
+      // 생성된 동 옵션 DOM에 추가
+      // <ul id="dongList" class="list hide">
+      //   <li class="option">${dongName}</li>
+      //   ...
+      // </ul>
       dongSelect.list.append(dongOptionElem);
     }
   } catch (err) {
     console.log(err);
   }
 };
+// 동 옵션들 초기화
 const resetDongOptions = () => {
   const dongOptionList = dongSelect.list;
+  // <ul id="dongList"></ul> 요소 내 자식 요소들 모두 삭제
   while (dongOptionList.hasChildNodes()) {
     dongOptionList.removeChild(dongOptionList.firstChild);
   }
@@ -131,11 +148,11 @@ window.addEventListener('load', async () => {
   dongDataForOptions = await parseDongDataForOptions();
   // navigation 메뉴 버튼 클릭 시 메뉴 모달 창 활성화
   navBtn.addEventListener('click', evt =>
-    handleNavModalOpen(evt, navModalOverlay),
+    handleModalOpen(evt, navModalOverlay),
   );
   // navigation 메뉴 모달 창 닫기 버튼 클릭 시 메뉴 모달 창 비활성화
   navModalCloseBtn.addEventListener('click', evt =>
-    handleNavModalOpen(evt, navModalOverlay),
+    handleModalOpen(evt, navModalOverlay),
   );
   // navigation 메뉴 모달 창 이외의 요소 클릭 시 메뉴 모달 창 비활성화
   navModalOverlay.addEventListener('click', e => {
