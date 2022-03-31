092f0a8212ee70085341a7fcd2b7963f15c2f696
ByungchanPark <pbc9236@gmail.com>
2022-04-01 08:34:09 +0900

    feat: 이메일 인증 모달 창 DOM에 추가

--- public/js/templates/Popup.js
+++ public/js/templates/Popup.js
@@ -3,14 +3,16 @@ import { createModalOverlay } from '../components/modal/Modal.js';
 import navModalElem from '../templates/NavModal.js';
 import searchModalElem from '../templates/SearchModal.js';
 import searchFilterModalElem from '../templates/SearchFilterModal.js';
+import emailVerifyModalElem from './EmailVerifyModal.js';
 
 const popupArea = createAreaElem('popup');
 // navigation 모달 오버레이 요소 생성
-const navModalOverlayElem = createModalOverlay();
+const modalOverlayElem = createModalOverlay();
 
 popupArea.appendChild(navModalElem);
-popupArea.appendChild(navModalOverlayElem);
 popupArea.appendChild(searchModalElem);
 popupArea.appendChild(searchFilterModalElem);
+popupArea.appendChild(emailVerifyModalElem);
+popupArea.appendChild(modalOverlayElem);
 
 document.body.appendChild(popupArea);
