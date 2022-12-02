c86bed61b4bde78b76009981b0a5c264c26f3b71
devRonPark <pbc9236@gmail.com>
2022-12-02 15:01:27 +0900

    [feat] backend 베이스 Url 변경

--- public/js/lib/constants.js
+++ public/js/lib/constants.js
@@ -2,5 +2,5 @@ export const nicknameRegExp = /^[가-힣]{3,16}$/;
 export const emailRegExp = /^[a-z0-9\.\-_]+@([a-z0-9\-]+\.)+[a-z]{2,6}$/;
 export const pwdRegExp =
   /^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?])*.{8,16}$/;
-export const backendBaseUrl = 'https://backend.jjincafe-in-seoul.com';
+export const backendBaseUrl = 'https://jjincafe-in-seoul.com';
 export const kakaoMapApiKey = 'f4e5b092174181f40919d82aa1363912';
